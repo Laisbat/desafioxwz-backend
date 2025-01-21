689ead83b04bd03fef2dac32d16773b4b63c9302
Laís Batista <lais.batista.pereira@gmail.com>
2025-01-21 14:13:12 -0300

    🎉 start project.

---
+++ src/main/java/com/desafioxwz/desafioxwz/DesafioxwzApplication.java
@@ -0,0 +1,13 @@
+package com.desafioxwz.desafioxwz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioxwzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioxwzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafioxwz/desafioxwz/DesafioxwzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioxwz.desafioxwz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioxwzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
